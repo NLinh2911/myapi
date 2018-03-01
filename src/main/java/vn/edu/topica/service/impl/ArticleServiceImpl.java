9261b02ca8a034224013badba70422314b88213d
NLinh2911 <linhnt1516@gmail.com>
2018-03-01 08:58:45 +0700

    update ArticleServiceRedisImpl

--- src/main/java/vn/edu/topica/service/impl/ArticleServiceImpl.java
+++ src/main/java/vn/edu/topica/service/impl/ArticleServiceImpl.java
@@ -1,6 +1,7 @@
 package vn.edu.topica.service.impl;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cache.annotation.CacheEvict;
 import org.springframework.cache.annotation.Cacheable;
 import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Service;
@@ -19,7 +20,7 @@ public class ArticleServiceImpl implements ArticleService {
     private ArticleRepository articleRepository;
 
     @Override
-    @Cacheable("articles")
+    @CacheEvict(value="articles")
     public List<Article> getAll() {
         List<Article> articles = new ArrayList<>();
         for(Article article: articleRepository.findAll()){
--- src/main/java/vn/edu/topica/service/impl/ArticleServiceRedisImpl.java
+++ src/main/java/vn/edu/topica/service/impl/ArticleServiceRedisImpl.java
@@ -1,15 +1,32 @@
 package vn.edu.topica.service.impl;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cache.annotation.CacheEvict;
+import org.springframework.cache.annotation.Cacheable;
+import org.springframework.context.annotation.Profile;
+import org.springframework.stereotype.Service;
 import vn.edu.topica.model.Article;
+import vn.edu.topica.repository.ArticleRepository;
 import vn.edu.topica.service.ArticleService;
 
+import java.util.ArrayList;
 import java.util.List;
 
+@Profile("redis-cache")
+@Service
 public class ArticleServiceRedisImpl implements ArticleService {
+
+    @Autowired
+    private ArticleRepository articleRepository;
 
     //TODO: Use Redis to cache
     @Override
+    @CacheEvict(value = "articles")
     public List<Article> getAll() {
-        return null;
+        List<Article> articles = new ArrayList<>();
+        for(Article article: articleRepository.findAll()){
+            articles.add(article);
+        }
+        return articles;
     }
 }
